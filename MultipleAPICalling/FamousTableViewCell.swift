ab8337cf95c42be140db9402e5383f22df3fb9b5
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-12 19:05:20 -0700

    Commit

---
+++ MultipleAPICalling/FamousTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FamousTableViewCell.swift
+//  MultipleAPICalling
+//
+//  Created by Arpit iOS Dev. on 12/06/24.
+//
+
+import UIKit
+
+class FamousTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MultipleAPICalling/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  MultipleAPICalling
+//
+//  Created by Arpit iOS Dev. on 12/06/24.
+//
+
+import Foundation
